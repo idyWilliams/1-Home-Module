d9873f7e888133cfb8dc7abe56c05d25d9047a17
Williams <widorenyin0@gmail.com>
2025-06-22 20:36:45 +0100

    Add home module components and screens

---
+++ screens/homescreen.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@/components/Restyle";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <Box alignItems="center" justifyContent="center" flex={1}>
+      <Text variant="bold16">Home Screen</Text>
+    </Box>
+  );
+};
+
+export default HomeScreen;
